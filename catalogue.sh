3a58589c669012b9096a26eb0e58344f2e47b937
ruthvekh <ruthvikk1214@gmail.com>
2026-02-22 16:46:49 +0530

    variables

--- catalogue.sh
+++ catalogue.sh
@@ -7,6 +7,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+SCRIPT_DIR=$PWD
+
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
@@ -62,7 +64,7 @@ cd /app
 npm install 
 VALIDATE $? " Downloading the dependencies"
 
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp /SCRPIT_DIR/catalogue.service /etc/systemd/system/catalogue.service
 VALIDATE $? "Creating catalogue service"
 
 systemctl daemon-reload
