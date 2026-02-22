7c3015b0baedfc760680a5518dac08da0fb12590
ruthvekh <ruthvikk1214@gmail.com>
2026-02-22 15:40:55 +0530

    variables

---
+++ catalogue.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+dnf module disable nodejs -y &>> $LOGS_FILE
+VALIDATE $? " Disabling the default nodejs version"
+
+dnf module enable nodejs:20 -y &>> $LOGS_FILE
+VALIDATE $? " Enabling the required nodejs version"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? " Installing the Nodejs"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "Creating app user"
+
+mkdir /app 
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+VALIDATE $? " Downloading the code"
+
+cd /app 
+Validate $? "Moving to app directory"
+
+unzip /tmp/catalogue.zip
+VALIDATE $? "Unzipping the app files in app directory"
+
+cd /app 
+
+npm install 
+VALIDATE $? " Downloading the dependencies"
